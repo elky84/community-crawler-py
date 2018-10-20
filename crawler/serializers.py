6e41ee63694c55362b82a9662fa48a962d38ee8f
Elky [SeongHoon Kim] <elky84@gmail.com>
2018-10-20 23:32:04 +0900

    시간 처리 기본 형식으로 처리하도록 수정.

--- crawler/serializers.py
+++ crawler/serializers.py
@@ -7,12 +7,11 @@ import datetime
 
 def payload_serializer(*, type: str, id: int = None, link: str, count: int,
                        title: str) -> dict:
-    utc_now = datetime.datetime.now(tz=datetime.timezone.utc)
     dic = {'type': type,
             'link': link,
             'count': count,
             'title': title,
-            'date': utc_now.isoformat()
+            'date': datetime.datetime.now(tz=datetime.timezone.utc)
             }
     
     dic['id'] = id
