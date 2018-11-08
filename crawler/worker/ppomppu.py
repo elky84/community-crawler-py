411a79fc2b56b533a938f1b3580dfa35a296832f
Elky [SeongHoon Kim] <elky84@gmail.com>
2018-11-09 00:14:14 +0900

    뽐뿌 크롤링도 기준 수정. (댓글 -> 조회수)

--- crawler/worker/ppomppu.py
+++ crawler/worker/ppomppu.py
@@ -37,10 +37,10 @@ class Ppomppu(BaseSite):
         for soup in self.crawler():
             for ctx in soup.select('table#revolution_main_table tr'):
                 if ctx.get('class') is not None and bool(re.match('list(0|1)', ctx.get('class')[0])):
-                    _temp = ctx.select('span.list_comment2')
-                    if len(_temp) != 0 and int(_temp[0].text) >= self.threshold:
+                    _count = int(ctx.select('td.eng.list_vspace')[3].text)
+                    
+                    if _count >= self.threshold:
                         _id = ctx.select('a')[1].get('href').split('no=')[1]
-                        _count = int(_temp[0].text.strip())
                         _title = ctx.select('a')[1].text
                         _link = self.url.split('zboard.php')[0] + ctx.select('a')[1].get('href')
                         obj = payload_serializer(type=self.type, id=_id, link=_link, count=_count, title=_title)
